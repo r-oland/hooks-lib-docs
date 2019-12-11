65566534296551ca83f8f62d07c8a96ccb5a93f4
Roland Branten <roland.branten@gmail.com>
2019-12-11 09:29:35 +0100

    form test 4

--- src/Component-groups/Form.js
+++ src/Component-groups/Form.js
@@ -24,14 +24,40 @@ export default function Form() {
       <form
         name="contact"
         method="post"
+        action="/thanks/"
         data-netlify="true"
         data-netlify-honeypot="bot-field"
       >
         <input type="hidden" name="form-name" value="contact" />
-        <input name="name" placeholder="Bert Jan" type="text" />
-        <br />
-        <br />
-        <SimpleButton as="button">Submit</SimpleButton>
+        <p hidden>
+          <label>
+            Don’t fill this out: <input name="bot-field" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your name:
+            <br />
+            <input type="text" name="name" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your email:
+            <br />
+            <input type="email" name="email" />
+          </label>
+        </p>
+        <p>
+          <label>
+            Message:
+            <br />
+            <textarea name="message" />
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
       </form>
       <Clipboard data-clipboard-text="vscode://file/d:/Freelancing/Code/Real projects/Components/src/Components/Raleway/Font-face.txt">
         <CopyButton>select</CopyButton>
