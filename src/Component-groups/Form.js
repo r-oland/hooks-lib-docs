a23816d99daf771779bcfff3862fce5440b2ba8c
Roland Branten <roland.branten@gmail.com>
2019-12-11 10:37:10 +0100

    test 11

--- src/Component-groups/Form.js
+++ src/Component-groups/Form.js
@@ -1,40 +1,78 @@
-// Components==============
+import { navigate } from "gatsby-link";
 import React from "react";
-import Clipboard from "react-clipboard.js";
-import styled from "styled-components";
-import { SimpleButton } from "../Components/SimpleButton";
-// =========================
 
-const Center = styled.div`
-  text-align: center;
-`;
+function encode(data) {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+}
 
-const Title = styled.h2`
-  text-align: center;
-`;
+export default function Contact() {
+  const [state, setState] = React.useState({});
 
-const CopyButton = styled(SimpleButton)`
-  margin: ${({ theme: { spacing } }) => `${spacing.s8}`} 0;
-`;
+  const handleChange = e => {
+    setState({ ...state, [e.target.name]: e.target.value });
+  };
 
-export default function Form() {
+  const handleSubmit = e => {
+    e.preventDefault();
+    const form = e.target;
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": form.getAttribute("name"),
+        ...state
+      })
+    })
+      .then(() => navigate(form.getAttribute("action")))
+      .catch(error => alert(error));
+  };
+
   return (
-    <Center>
-      <Title>Form</Title>
+    <div>
+      <h2>Contact</h2>
       <form
         name="contact"
         method="post"
+        action="/success/"
         data-netlify="true"
         data-netlify-honeypot="bot-field"
+        onSubmit={handleSubmit}
       >
+        {/* The `form-name` hidden field is required to support form submissions without JavaScript */}
         <input type="hidden" name="form-name" value="contact" />
-        <input type="email" name="email" />
-        <textarea name="message" />
-        <button type="submit">Send</button>
+        <p hidden>
+          <label>
+            Don’t fill this out:{" "}
+            <input name="bot-field" onChange={handleChange} />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your name:
+            <br />
+            <input type="text" name="name" onChange={handleChange} />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your email:
+            <br />
+            <input type="email" name="email" onChange={handleChange} />
+          </label>
+        </p>
+        <p>
+          <label>
+            Message:
+            <br />
+            <textarea name="message" onChange={handleChange} />
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
       </form>
-      <Clipboard data-clipboard-text="vscode://file/d:/Freelancing/Code/Real projects/Components/src/Components/Raleway/Font-face.txt">
-        <CopyButton>select</CopyButton>
-      </Clipboard>
-    </Center>
+    </div>
   );
 }
