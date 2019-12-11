b94aee220bf9aca11b3eeb866a8b57a86a60d975
Roland Branten <roland.branten@gmail.com>
2019-12-11 09:20:59 +0100

    test3

--- src/Component-groups/Form.js
+++ src/Component-groups/Form.js
@@ -22,12 +22,11 @@ export default function Form() {
     <Center>
       <Title>Form</Title>
       <form
-        name="test-form"
+        name="contact"
         method="post"
         data-netlify="true"
         data-netlify-honeypot="bot-field"
       >
-        <input type="hidden" name="bot-field" />
         <input type="hidden" name="form-name" value="contact" />
         <input name="name" placeholder="Bert Jan" type="text" />
         <br />
