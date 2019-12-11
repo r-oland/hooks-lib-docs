191a5d4ed56a57c8bbd6e02e097f5a4dae5022f2
Roland Branten <roland.branten@gmail.com>
2019-12-11 09:15:26 +0100

    test2

--- src/Component-groups/Form.js
+++ src/Component-groups/Form.js
@@ -27,6 +27,8 @@ export default function Form() {
         data-netlify="true"
         data-netlify-honeypot="bot-field"
       >
+        <input type="hidden" name="bot-field" />
+        <input type="hidden" name="form-name" value="contact" />
         <input name="name" placeholder="Bert Jan" type="text" />
         <br />
         <br />
