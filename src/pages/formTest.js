8b64cc6eaffc1c1b2d64e9f28b208e3926cd042d
Roland Branten <roland.branten@gmail.com>
2020-01-31 18:07:30 +0100

    formTest2

--- src/pages/formTest.js
+++ src/pages/formTest.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Form from "../Component-groups/Form";
+import StoryForm from "../Component-groups/StoryForm";
 import Layout from "../Layout/Layout";
 
 export default function Contact() {
   return (
     <Layout>
       <Form />
+      <StoryForm />
     </Layout>
   );
 }
